e832be902f9403d2f57909d32fee4138f8d26041
Hank Tom <hanktom@gmail.com>
2018-09-05 14:45:23 +0800

    Activity 的生命週期

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
         findViews();
         bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +40,42 @@ public class MainActivity extends AppCompatActivity {
         bHelp = findViewById(R.id.b_help);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+    }
+
     public void bmi(View v) {
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
@@ -46,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight/(height*height);
         Intent intent = new Intent(this, ResultActivity.class);
-        intent.putExtra("BMI_EXTRA", bmi);
+        intent.putExtra(getString(R.string.bmi_extra), bmi);
         startActivity(intent);
 
         /*Log.d("BMI", String.valueOf(bmi));
