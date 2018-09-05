aa74a7a9f4975f19ba9ced07a2c24e5e708b3eec
Hank Tom <hanktom@gmail.com>
2018-09-05 14:28:34 +0800

    Toast, AlertDialog

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,9 +1,13 @@
 package com.tom.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +26,22 @@ public class MainActivity extends AppCompatActivity {
         edWeight = findViewById(R.id.ed_weight);
         edHeight = findViewById(R.id.ed_height);
         bHelp = findViewById(R.id.b_help);
+    }
+
+    public void bmi(View v) {
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("BMI", String.valueOf(bmi));
+        Toast.makeText(this, String.valueOf(bmi), Toast.LENGTH_LONG)
+                .show();
+        new AlertDialog.Builder(this)
+                .setMessage(bmi + "")
+                .setTitle("BMI運算")
+                .setPositiveButton("OK", null)
+                .setNeutralButton("Cancel", null)
+                .show();
     }
 }
